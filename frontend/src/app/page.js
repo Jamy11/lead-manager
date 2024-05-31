49dd203a67efdf30cbb0e59771323f62f8091c4a
jamy11 <jamyarrahman82@gmail.com>
2024-05-31 16:23:19 -0230

    bootstrap installed

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,25 +2,30 @@
 import Image from "next/image";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Header from "@/components/layouts/Header";
 export default function Home() {
-  const [data, SetData] = useState("");
+  // const [data, SetData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:8000/api/leads/")
-      .then(function (response) {
-        // handle success
-        SetData(response.data);
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://127.0.0.1:8000/api/leads/")
+  //     .then(function (response) {
+  //       // handle success
+  //       SetData(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
+  //       // always executed
+  //     });
+  // }, []);
 
-  return <>{data.name}</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
